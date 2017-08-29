48a719b7a49c9cd020a1c93ad4420f85808408b4
gplane <g-plane@hotmail.com>
2017-08-29 17:51:00 +0800

    Fix deployment

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -12,26 +12,33 @@ if (!fs.existsSync(resolve('screenshots'))) {
 }
 
 async function gotoPage (url, browser) {
-  const page = await browser.newPage()
-  await page.setViewport({ width: 1920, height: 1080 })
-  await page.goto(url)
-  return page
+  try {
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1920, height: 1080 })
+    await page.goto(url)
+    return page
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 ;(async () => {
   const browser = await puppeteer.launch()
-  let page = await browser.newPage()
-  page = await gotoPage('http://localhost:3000', browser)
+  let page = await gotoPage('http://localhost:3000', browser)
   await page.screenshot({ path: './screenshots/User_Index.png' })
+  await page.close()
 
   page = await gotoPage('http://localhost:3000/#/user/closet', browser)
   await page.screenshot({ path: './screenshots/User_Closet.png' })
+  await page.close()
 
   page = await gotoPage('http://localhost:3000/#/user/player', browser)
   await page.screenshot({ path: './screenshots/User_Player.png' })
+  await page.close()
 
   page = await gotoPage('http://localhost:3000/#/user/profile', browser)
   await page.screenshot({ path: './screenshots/User_Profile.png' })
+  await page.close()
 
   browser.close()
   mock.close()
