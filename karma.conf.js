04a605684622b446c694b7286c63433a6ff14ff9
gplane <g-plane@hotmail.com>
2017-09-16 20:48:32 +0800

    Disable browser logging

--- karma.conf.js
+++ karma.conf.js
@@ -7,58 +7,28 @@ const webpackConfig = {
 
 module.exports = function (config) {
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
     plugins: [
       'karma-jasmine',
       'karma-webpack',
       'karma-chrome-launcher',
       'karma-firefox-launcher'
     ],
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-    // list of files / patterns to load in the browser
     files: ['specs/**/*.spec.js'],
-
-    // list of files to exclude
     exclude: [],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors:
-    // https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'specs/**/*.spec.js': ['webpack']
     },
     webpack: webpackConfig,
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
-
-    // web server port
     port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-    // level of logging
     logLevel: config.LOG_INFO,
-
+    browserConsoleLogOptions: { terminal: false },
     autoWatch: true,
-
     browsers: ['ChromeHeadless', 'Firefox'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity
   })
 }
